53e2b36e9a72ea787deeffbd1c56d726684083fc
IB <info@ibco.ltd>
2025-02-21 12:20:16 +0900

    Fix Rails version in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,5 @@
 require_relative "boot"
 
-require "rails/all"
-config.require_master_key = false
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,7 +8,7 @@ Bundler.require(*Rails.groups)
 module DispatchApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 7.2
     config.i18n.default_locale = :ja
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
