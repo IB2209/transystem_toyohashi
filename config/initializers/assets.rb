92515339738ae8cf47ebaff7c4636b18486025fa
IB <info@ibco.ltd>
2025-02-24 16:09:32 +0900

    コミット内容

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+
+
+if Rails.application.config.respond_to?(:assets)
+  Rails.application.config.assets.version = '1.0'
+  Rails.application.config.asset_host = ENV['ASSET_HOST'] if ENV['ASSET_HOST'].present?
+end
+
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
