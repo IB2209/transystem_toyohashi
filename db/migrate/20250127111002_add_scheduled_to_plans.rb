f09980ca93766ccd47cdf0f44b8e204815646f9e
IB <info@ibco.ltd>
2025-02-24 16:46:47 +0900

    コミット内容

--- db/migrate/20250127111002_add_scheduled_to_plans.rb
+++ db/migrate/20250127111002_add_scheduled_to_plans.rb
@@ -1,5 +1,7 @@
-class AddScheduledToPlans < ActiveRecord::Migration[6.1]
+class AddScheduledToPlans < ActiveRecord::Migration[7.2]
   def change
-    add_column :plans, :scheduled, :boolean, default: false, null: false
+    unless column_exists?(:plans, :scheduled)
+      add_column :plans, :scheduled, :boolean, default: false, null: false
+    end
   end
 end
