b9346ca10dba99e1ed517e90b54dca2f44ac007c
IB <info@ibco.ltd>
2025-02-25 14:06:41 +0900

    コミット内容

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,2 @@
 # Rails.application.config.assets.version は、アセットの変更を認識させるためのもの
 Rails.application.config.assets.version = "1.0"
-
-# 個別のアセットをプリコンパイル対象に追加
-#Rails.application.config.assets.precompile += %w( script.js movement_records.js )
