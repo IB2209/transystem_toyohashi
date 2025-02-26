3e275a1682b82165b116c3fb8f384e5c80990c68
IB <info@ibco.ltd>
2025-02-27 01:55:45 +0900

    Importmap の修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "@rails/actioncable";
-import Rails from "@rails/ujs";
-Rails.start();
+import "@rails/actioncable"
+import Rails from "@rails/ujs"
+Rails.start()
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
