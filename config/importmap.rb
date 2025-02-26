1d7ed249d8829d3a34c07f35ecc16888ddff34bf
IB <info@ibco.ltd>
2025-02-27 02:07:58 +0900

    Importmap の修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -14,3 +14,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 # Import additional libraries
 pin "@hotwired/turbo", to: "turbo.min.js", preload: true
 pin "@rails/actioncable", to: "actioncable.js", preload: true
+pin "@rails/ujs", to: "rails-ujs.js", preload: true
