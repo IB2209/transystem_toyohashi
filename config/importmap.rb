a5e9be3543865633baf5b3c27de26306c006b3fb
IB <info@ibco.ltd>
2025-02-26 13:28:07 +0900

    コミット内容

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,12 @@
-# Pin npm packages by running ./bin/importmap
+# config/importmap.rb
 
+# Import the main application JavaScript file
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "@hotwired/turbo-rails", to: "turbo.js"
+
+# Import Turbo and Stimulus (if using Hotwire)
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Import controllers from app/javascript/controllers
 pin_all_from "app/javascript/controllers", under: "controllers"
