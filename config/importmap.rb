fdbb86e839c246f6d9013cfc912158e8995e8b85
IB <info@ibco.ltd>
2025-02-27 16:57:45 +0900

    コミット内容

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,15 +1,2 @@
 # Import the main application JavaScript file
 pin "application"
-
-# Import Turbo and Stimulus (if using Hotwire)
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.12
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.12
-
-# Import additional libraries
-pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.0.100
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
-
-# Import vendor JavaScript
-pin_all_from "vendor/javascript", under: "vendor"
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.2.201
