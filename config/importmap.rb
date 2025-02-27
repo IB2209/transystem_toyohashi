329a22bd355b0b713a554b338570cd75cb87dcc8
IB <info@ibco.ltd>
2025-02-27 13:25:48 +0900

    コミット内容

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,9 +6,6 @@ pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.12
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.12
 
-# Import controllers from app/javascript/controllers
-pin_all_from "app/javascript/controllers", under: "controllers"
-
 # Import additional libraries
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.0.100
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
