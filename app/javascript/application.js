ec83c0691438ecb52e887057c04df6886300c6df
IB <info@ibco.ltd>
2025-02-27 16:59:13 +0900

    コミット内容

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "@rails/actioncable"
-import "@hotwired/turbo"
-import Rails from "@rails/ujs"
-Rails.start()
-import { Application } from "@hotwired/stimulus"
-
 const application = Application.start()
 
 document.addEventListener("DOMContentLoaded", () => {
