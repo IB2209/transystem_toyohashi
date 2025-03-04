9d2ba3d7ad16326f12299ede70c48863dc7a9094
IB <info@ibco.ltd>
2025-03-04 16:16:09 +0900

    コミット内容

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -109,3 +109,14 @@ function initTabs() {
     });
   });
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service_worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, function(err) {
+        console.log('Service Worker registration failed:', err);
+      });
+  });
+}
