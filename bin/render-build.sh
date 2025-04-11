ba37324740ce8553f9302b502dd21db9bd764b19
IB <info@ibco.ltd>
2025-04-11 11:17:45 +0900

    ◯◯を修正しました

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,15 +4,11 @@ set -o errexit
 echo "📦 Installing wkhtmltopdf..."
 
 mkdir -p .apt
-curl -L https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox_0.12.6-1.bionic_amd64.deb -o wkhtmltopdf.deb
+curl -L -o wkhtmltopdf.deb https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox_0.12.6-1.bionic_amd64.deb
 dpkg -x wkhtmltopdf.deb .apt
 rm wkhtmltopdf.deb
 
 echo "📦 Installing Ruby dependencies..."
-
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-
-# DBマイグレーション（必要に応じて）
-# bundle exec rails db:migrate
