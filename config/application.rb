9b319f877ec020625302cd7e9017e19336d24db3
IB <info@ibco.ltd>
2025-06-17 12:28:33 +0900

    ◯◯を修正しました

--- config/application.rb
+++ config/application.rb
@@ -10,22 +10,16 @@ require "importmap-rails"
 
 module DispatchApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
     config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'  # 日本時間（JST）を設定
-    config.active_record.default_timezone = :local  # DBのタイムゾーンもローカルに設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
-    # `lib/` 以下の不要なフォルダをオートロードしない
+    # サブディレクトリ対応（★この行を追加）
+    config.relative_url_root = "/coxgear/transystem/toyohashi"
+
+    # 不要なlibフォルダのオートロードを無視
     config.autoload_lib(ignore: %w[assets tasks])
-
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
